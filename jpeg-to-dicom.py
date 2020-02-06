7c59f43d433c16b6d7b8191753949205a4636624
Jan Witowski <jwitos@gmail.com>
2020-02-06 16:05:28 -0500

    Create jpeg-to-dicom.py

---
+++ jpeg-to-dicom.py
@@ -0,0 +1,72 @@
+import pydicom
+from pydicom.dataset import Dataset, FileDataset
+from pydicom.uid import generate_uid
+from PIL import Image
+import numpy
+import uuid
+
+# Load image with Pillow
+img = Image.open(self.f)
+width, height = img.size
+print("File format is {} and size: {}, {}".format(img.format, width, height))
+
+# Handling PNG and BMP files
+# Pillow conversion to RGB
+if img.format == 'PNG' or img.format == 'BMP':
+    img = img.convert('RGB')
+
+# Handling multiple image modes (types/depth of pixels)
+# Docs: https://pillow.readthedocs.io/en/3.1.x/handbook/concepts.html
+if img.mode == 'L':
+    np_frame = ds.PixelData = np_frame.tobytes()
+elif img.mode == 'RGBA' or img.mode == 'RGB':
+    np_frame = numpy.array(img.getdata(), dtype=numpy.uint8)
+else:
+    print("Unknown image mode")
+    return
+
+# Name for saved DICOM
+dicomized_filename = str(uuid.uuid4()) + '.dcm'
+
+# Generate required UID metatags
+SOPClassUID = generate_uid()
+SOPInstanceUID = generate_uid()
+StudyInstanceUID = generate_uid()
+SeriesInstanceUID = generate_uid()
+
+# Create DICOM from scratch
+ds = Dataset()
+ds.file_meta = Dataset()
+ds.file_meta.TransferSyntaxUID = pydicom.uid.ExplicitVRLittleEndian
+ds.file_meta.MediaStorageSOPClassUID = '1.2.840.10008.5.1.4.1.1.1.1'
+ds.file_meta.MediaStorageSOPInstanceUID = "1.2.3"
+ds.file_meta.ImplementationClassUID = "1.2.3.4"
+
+ds.PatientName = 'Created'
+
+ds.Rows = img.height
+ds.Columns = img.width
+#ds.PhotometricInterpretation = "MONOCHROME1"
+ds.PhotometricInterpretation = "YBR_FULL_422"
+if np_frame.shape[1] == 3:
+    ds.SamplesPerPixel = 3
+else:
+    ds.SamplesPerPixel = 1
+ds.BitsStored = 8
+ds.BitsAllocated = 8
+ds.HighBit = 7
+ds.PixelRepresentation = 0
+ds.PlanarConfiguration = 0
+ds.NumberOfFrames = 1
+
+ds.SOPClassUID = generate_uid()
+ds.SOPInstanceUID = generate_uid()
+ds.StudyInstanceUID = generate_uid()
+ds.SeriesInstanceUID = generate_uid()
+
+ds.PixelData = np_frame
+
+ds.is_little_endian = True
+ds.is_implicit_VR = False
+
+ds.save_as(dicomized_filename, write_like_original=False)
